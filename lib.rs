a66c1dee553d49508fb9cc1361db88ceceb70afb
shin.takahashi <realtakahashi.0924@gmail.com>
2021-01-10 21:21:18 +0900

    update ink version

--- lib.rs
+++ lib.rs
@@ -2,56 +2,68 @@
 
 use ink_lang as ink;
 
-#[ink::contract(version = "0.1.0")]
+#[ink::contract]
 mod get_plasm_my_number {
-    use ink_core::storage;
-
+    #[cfg(not(feature = "ink-as-dependency"))]
+    use ink_storage::collections::HashMap as StorageHashMap;
     /// Defines the storage of your contract.
     /// Add new fields to the below struct in order
     /// to add new static storage fields to your contract.
     #[ink(storage)]
-    struct GetPlasmMyNumber {
-        numbers: storage::HashMap<AccountId, u128>,
-        present_number: storage::Value<u128>,
+    pub struct GetPlasmMyNumber {
+        /// Stores a single `bool` value on the storage.
+        numbers: StorageHashMap<AccountId, u128>,
+        present_number: u128,
+ 
     }
 
     impl GetPlasmMyNumber {
         /// Constructor that initializes the `bool` value to the given `init_value`.
         #[ink(constructor)]
-        fn new(&mut self) {
-            self.present_number.set(0);
+        pub fn new() -> Self {
+            Self { numbers:StorageHashMap::new(),present_number:0 }
+        }
+
+        /// Constructor that initializes the `bool` value to `false`.
+        ///
+        /// Constructors can delegate to other constructors.
+        #[ink(constructor)]
+        pub fn default() -> Self {
+            Self::new()
         }
 
         /// get plasm my number
         #[ink(message)]
-        fn get_plasm_my_number(&mut self) {
+        pub fn get_plasm_my_number(&mut self) {
             let caller = self.env().caller();
             if self.check_plasm_my_number(caller) == 0 {
-                self.present_number.set(self.present_number.get() + 1);
-                self.numbers.insert(caller, *self.present_number.get());
+                self.present_number = self.present_number + 1;
+                self.numbers.insert(caller, self.present_number);
             }
         }
 
         /// confirm my own number
-        #[ink(message)]
-        fn confirm_plasm_my_number(&self) -> u128 {
-            let caller = self.env().caller();
-            self.check_plasm_my_number(caller)
-        }
+        // #[ink(message)]
+        // pub fn confirm_plasm_my_number(&self) -> u128 {
+        //     let caller = self.env().caller();
+        //     self.check_plasm_my_number(caller)
+        // }
 
         /// get present number
         #[ink(message)]
-        fn get_present_number(&self) -> u128 {
-            *self.present_number.get()
+        pub fn get_present_number(&self) -> u128 {
+            self.present_number
         }
 
         /// check my plasm number. If you have not get number, you get zero value.
-        fn check_plasm_my_number(&self, address: AccountId) -> u128 {
+        pub fn check_plasm_my_number(&self, address: AccountId) -> u128 {
             match self.numbers.get(&address) {
                 Some(result) => *result,
                 None => 0,
             }
         }
+
+
     }
 
     /// Unit tests in Rust are normally defined within such a `#[cfg(test)]`
